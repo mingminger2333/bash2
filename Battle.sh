d692886874d9f7048c90bd78d82aa81b4651793d
mingminger2333 <37793579+mingminger2333@users.noreply.github.com>
2018-03-26 15:46:00 +0800

    Create Battle.sh

---
+++ Battle.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -f *.xml *.sql
+wget ftp://chanpin:chanpin_fangan@122.226.207.236:56118/wsz_tx/B*
+server=wsz_tx_center
+_sql(){
+        sed -i "s/\xEF\xBB\xBF//g" *.sql
+        for i in `ls -l *.sql|awk '{print $NF}'`;do j=`echo $i|cut -d "." -f 1`;mysql $server -e "delete from $j;"; mysql $server < $i ;done
+}
+_xml(){
+        [ -f BattleActivity.xml ] &&  \cp -avf BattleActivity.xml /data/home/wangxian/wsz_tx_center/release/Config/
+}
+ls |grep -E "B*.sql" && _sql
+ls |grep -E "B*.xml" && _xml
+sh /data/scripts/updateAdmin.sh restart
